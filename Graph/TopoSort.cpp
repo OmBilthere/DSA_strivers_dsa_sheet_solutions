0a262924c40d49b4a7cbaa59b32461bb6779c949
OM BILTHERE <ombilthere953@gmail.com>
2026-01-15 18:52:58 +0000

    18 Questions Done

---
+++ Graph/TopoSort.cpp
@@ -0,0 +1,58 @@
+class Solution {
+  public:
+  
+    void dfs (int node , vector<int> & vis , vector<vector<int>>& adj , stack<int>& st) {
+        
+        vis[node] = 1;
+        
+        for(auto it: adj[node]) {
+            
+            if (!vis[it]) 
+            
+            dfs( it , vis , adj , st);
+            
+        }
+        
+        st.push(node);
+        
+    }
+    
+    
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        
+        vector<int> vis (V , 0 );
+        
+        stack<int> st;
+        
+        vector<vector<int>> adj(V);
+        
+        for(int i = 0 ; i < edges.size() ; i++ ) {
+            
+            int u = edges[i][0];
+            
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+            
+          
+        }
+        
+        for( int i = 0 ; i < V ; i ++ ) {
+            
+            if( !vis[i] )
+            
+            dfs( i , vis , adj , st );
+        }
+        
+        vector<int> ans;
+        
+        while( !st.empty()) {
+            
+            ans.push_back(st.top());
+            
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
